e649f18d8dc1758b831b186f78bef422b48d1bef
Martin Anderson <martins.andersons@hotmail.com>
2019-03-13 01:13:12 +0200

    remove imports, format fit_data

--- lib/fit_kit.dart
+++ lib/fit_kit.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:fit_kit/fit_data.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class FitKit {
